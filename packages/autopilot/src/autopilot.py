20ec43c993ca663685ab226d10786fb9ae4f62d5
matt439 <matt439@gmail.com>
2025-05-24 17:56:29 +1000

    update K_THETA to 50.0 for improved lane following control

--- packages/autopilot/src/autopilot.py
+++ packages/autopilot/src/autopilot.py
@@ -27,7 +27,7 @@ LANE_CONTROLLER_NODE_THETA_THRES_MAX = "/vader/lane_controller_node/theta_thres_
 
 V_BAR = 0.3 # clamped from 0 to 5
 K_D = 0.0 # clamped from -100 to 100
-K_THETA = 0.0 # clamped from -100 to 100
+K_THETA = 50.0 # clamped from -100 to 100
 K_ID = 0.0 # clamped from -100 to 100
 K_IPHI = 0.0 # clamped from -100 to 100
 THETA_THRES_MIN = 0.0 # clamped from -100 to 100
