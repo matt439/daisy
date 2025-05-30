33f346927ec8c95d1cc9f254e07344ea4a7ae5c6
matt439 <matt439@gmail.com>
2025-05-30 18:05:26 +1000

    fix bug when calling update

--- packages/movement_controller/src/movement_controller.py
+++ packages/movement_controller/src/movement_controller.py
@@ -198,7 +198,7 @@ class MovementController:
     def run(self):
         rate = rospy.Rate(MOVEMENT_CONTROLLER_UPDATE_FREQUENCY)
         while not rospy.is_shutdown():
-            self._movement_controller.update()
+            self.update()
             rate.sleep()
 
 class MovementControllerState(ABC):
