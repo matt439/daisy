c4cbe7b1e2ce1d80bfae925d9bed7bfe5e46f893
matt439 <matt439@gmail.com>
2025-05-30 12:34:25 +1000

    only publish status when there is change to obstacle status

--- packages/obstacle_detector/src/obstacle_detector.py
+++ packages/obstacle_detector/src/obstacle_detector.py
@@ -22,19 +22,22 @@ class ObstacleDetector:
         # Printing to the terminal, ROS style
         rospy.loginfo("Initalized obstacle_detector node!")
 
+    def publish_obstacle_status(self):
+        # Publish the current obstacle detection status
+        self.pub_obstacle.publish(self.convert_to_int8(self._obstacle_detected))
+
     def callback_tof(self, msg):
         # Check if the distance is less than the threshold
         if msg.range < OBSTACLE_DISTANCE_THRESHOLD:
             if self._obstacle_detected == False: # Only log when the state changes
                 rospy.loginfo("Obstacle detected!")
+                self.publish_obstacle_status()
             self._obstacle_detected = True
         else:
             if self._obstacle_detected == True: # Only log when the state changes
                 rospy.loginfo("Obstacle cleared!")
+                self.publish_obstacle_status()
             self._obstacle_detected = False
-
-        # Publish the obstacle detection status
-        self.pub_obstacle.publish(self.convert_to_int8(self._obstacle_detected))
 
     def convert_to_int8(self, value):
         if value:
