ce18d562bb96053c972a1cec4088e5afc18d856c
matt439 <matt439@gmail.com>
2025-05-31 19:44:29 +1000

    refactor MovementController to adjust overtaking maneuver duration and forward distance for improved performance

--- packages/movement_controller/src/movement_controller.py
+++ packages/movement_controller/src/movement_controller.py
@@ -11,10 +11,10 @@ MOVEMENT_CONTROLLER_UPDATE_FREQUENCY = 20.0  # Hz
 
 WHEEL_VELOCITY_STOPPED_THRESHOLD = 0.01  # m/s, threshold to consider the wheel stopped
 
-OVERTAKING_MANEUVER_DURATION = 13.0  # seconds, duration of the overtaking maneuver
+OVERTAKING_MANEUVER_DURATION = 9.0  # seconds, duration of the overtaking maneuver
 OVERTAKING_TIMEOUT_DURATION = 15.0  # seconds
-OVERTAKING_FORWARD_DISTANCE = 0.8  # meters
-OVERTAKING_MIDWAY_DISTANCE = 0.58 # meters, where the piecewise function is split into two parts 
+OVERTAKING_FORWARD_DISTANCE = 0.5  # meters
+OVERTAKING_MIDWAY_DISTANCE = 0.28 # meters, where the piecewise function is split into two parts 
 AXLE_LENGTH = 0.1
 OVERTAKING_WHEEL_OFFSET = 0.09  # meters
 # Generalized logistic function parameters
